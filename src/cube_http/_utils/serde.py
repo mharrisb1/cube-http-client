046d3c39b348d1badad97fdb8b888c6bb9688f07
Michael Harris <mharris@luabase.com>
2024-07-30 16:32:46 -0400

    feat(utils): add serde utils to support pydantic v1 and v2

---
+++ src/cube_http/_utils/serde.py
@@ -0,0 +1,21 @@
+from typing import Any, Type, TypeVar
+
+from pydantic import BaseModel
+
+__all__ = ["model_dict", "model_parse"]
+
+M = TypeVar("M", bound=BaseModel)
+
+
+def model_dict(m: BaseModel, **kwargs: Any) -> dict[str, Any]:
+    if hasattr(m, "model_dump"):
+        return getattr(m, "model_dump")(**kwargs)
+    else:
+        return getattr(m, "dict")(**kwargs)
+
+
+def model_parse(m: Type[M], d: object) -> M:
+    if hasattr(m, "model_validate"):
+        return getattr(m, "model_validate")(d)
+    else:
+        return getattr(m, "parse_obj")(d)
