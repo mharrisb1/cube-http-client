52d61ec8dfb1886cb4efb9cce01fdd1148dfb332
Michael Harris <mharris@luabase.com>
2024-08-01 12:46:44 -0400

    feat(utils): add kwargs for passthrough

--- src/cube_http/_utils/serde.py
+++ src/cube_http/_utils/serde.py
@@ -14,8 +14,8 @@ def model_dict(m: BaseModel, **kwargs: Any) -> dict[str, Any]:
         return getattr(m, "dict")(**kwargs)
 
 
-def model_parse(m: Type[M], d: object) -> M:
+def model_parse(m: Type[M], d: object, **kwargs: Any) -> M:
     if hasattr(m, "model_validate"):
-        return getattr(m, "model_validate")(d)
+        return getattr(m, "model_validate")(d, **kwargs)
     else:
-        return getattr(m, "parse_obj")(d)
+        return getattr(m, "parse_obj")(d, **kwargs)
