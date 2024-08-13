c2337da63dc7c37f3f856d0243c951f4da94cc47
Michael Harris <mharris@luabase.com>
2024-08-13 11:39:15 -0400

    feat(types): better defined types based on documentation

--- src/cube_http/types/v1/load.py
+++ src/cube_http/types/v1/load.py
@@ -1,141 +1,250 @@
-from typing import Any, Dict, List, Optional, TypedDict, Union
+from typing import Any, Dict, List, Literal, Optional, Tuple, TypedDict, Union
 from typing_extensions import NotRequired
 
 from pydantic import BaseModel, Field
 
+from ..._utils.recursive import model_build_recursive
+
+from .meta import (
+    V1CubeMetaDimension,
+    V1CubeMetaMeasure,
+    V1CubeMetaSegment,
+)
+
 
 class V1LoadResultAnnotation(BaseModel):
-    measures: Dict[str, Any] = Field(description="Annotations for measures")
-    dimensions: Dict[str, Any] = Field(description="Annotations for dimensions")
-    segments: Dict[str, Any] = Field(description="Annotations for segments")
-    time_dimensions: Dict[str, Any] = Field(
-        alias="timeDimensions", description="Annotations for time dimensions"
+    measures: Dict[str, V1CubeMetaMeasure]
+    """Annotations for measures in the result."""
+
+    dimensions: Dict[str, V1CubeMetaDimension]
+    """Annotations for dimensions in the result."""
+
+    segments: Dict[str, V1CubeMetaSegment]
+    """Annotations for segments in the result."""
+
+    time_dimensions: Dict[str, V1CubeMetaDimension] = Field(
+        alias="timeDimensions"
     )
+    """Annotations for time dimensions in the result."""
 
 
 class V1LoadResult(BaseModel):
-    data_source: Optional[str] = Field(
-        alias="dataSource",
-        default=None,
-        description="Data source of the load result",
+    query: Optional["V1LoadRequestQuery"] = None
+    """The original query object."""
+
+    data: List[Dict[str, Any]]
+    """Data returned by the load result."""
+
+    last_refresh_time: Optional[str] = Field(
+        alias="lastRefreshTime", default=None
     )
-    annotation: V1LoadResultAnnotation = Field(
-        description="Annotations for the load result"
+    """Timestamp indicating the last time the query was refreshed."""
+
+    refresh_key_values: Optional[List[List[Dict[str, Any]]]] = Field(
+        alias="refreshKeyValues", default=None
     )
-    data: List[Dict[str, str]] = Field(description="Data of the load result")
+    """Values of the refresh key used for the query."""
+
+    used_pre_aggregations: Optional[Dict[str, Any]] = Field(
+        alias="usedPreAggregations", default=None
+    )
+    """Information on pre-aggregations used in the query."""
+
+    transformed_query: Optional[Dict[str, Any]] = Field(
+        alias="transformedQuery", default=None
+    )
+    """The query after being transformed, usually for optimization purposes."""
+
+    request_id: Optional[str] = Field(alias="requestId", default=None)
+    """Unique identifier for the request."""
+
+    annotation: V1LoadResultAnnotation
+    """Annotations for the load result."""
+
+    data_source: Optional[str] = Field(alias="dataSource", default=None)
+    """The data source from which the result was loaded."""
+
+    db_type: Optional[str] = Field(alias="dbType", default=None)
+    """The type of database queried."""
+
+    ext_db_type: Optional[str] = Field(alias="exDbType", default=None)
+    """External database type, if applicable."""
+
+    external: Optional[bool] = None
+    """Indicates if the query result was fetched from an external source."""
+
+    slow_query: Optional[bool] = Field(alias="slowQuery", default=None)
+    """Indicates if the query is considered slow."""
 
 
 class V1LoadResponse(BaseModel):
-    pivot_query: Optional[Dict[str, Any]] = Field(
-        alias="pivotQuery",
-        default=None,
-        description="Pivot query of the load response",
+    pivot_query: Optional["V1LoadRequestQuery"] = Field(
+        alias="pivotQuery", default=None
     )
-    slow_query: Optional[bool] = Field(
-        alias="slowQuery",
-        default=None,
-        description="Indicates if the query is slow",
-    )
-    query_type: Optional[str] = Field(
-        alias="queryType", default=None, description="Type of the query"
-    )
-    results: List[V1LoadResult] = Field(
-        description="Results of the load response"
-    )
+    """The pivot query associated with the load response."""
+
+    query_type: Optional[str] = Field(alias="queryType", default=None)
+    """Type of the query, primarily for internal use."""
+
+    slow_query: Optional[bool] = Field(alias="slowQuery", default=None)
+    """Indicates if the query was considered slow."""
+
+    results: List[V1LoadResult]
+    """List of results obtained from the load response."""
 
 
-class V1LoadRequestQueryFilterLogicalAnd(BaseModel):
-    and_: Optional[List[Dict[str, Any]]] = Field(
-        default=None, alias="and", description="AND filter logic"
-    )
-
-
-class V1LoadRequestQueryFilterLogicalOr(BaseModel):
-    or_: Optional[List[Dict[str, Any]]] = Field(
-        default=None, alias="or", description="OR filter logic"
-    )
+V1LoadRequestQueryFilterOperator = Literal[
+    "equals",
+    "notEquals",
+    "contains",
+    "notContains",
+    "startsWith",
+    "notStartsWith",
+    "endsWith",
+    "notEndsWith",
+    "gt",
+    "gte",
+    "lt",
+    "lte",
+    "set",
+    "notSet",
+    "inDateRange",
+    "notInDateRange",
+    "beforeDate",
+    "afterDate",
+    "measureFilter",
+]
 
 
 class V1LoadRequestQueryFilterBase(BaseModel):
-    member: Optional[str] = Field(default=None, description="Member to filter")
-    operator: Optional[str] = Field(default=None, description="Filter operator")
-    values: Optional[List[str]] = Field(
-        default=None, description="Values for the filter"
+    member: Optional[str] = None
+    """Dimension or measure to be used in the filter, e.g., `stories.isDraft`. 
+    Differentiates between filtering dimensions and filtering measures."""
+
+    operator: Optional[V1LoadRequestQueryFilterOperator] = None
+    """Operator to apply in the filter. 
+    Some operators are exclusive to measures, while others depend on the dimension type."""
+
+    values: Optional[List[str]] = None
+    """List of values for the filter, provided as strings. 
+    For dates, use the `YYYY-MM-DD` format."""
+
+
+class V1LoadRequestQueryFilterLogicalAnd(BaseModel):
+    and_: Optional[List["V1LoadRequestQueryFilterItem"]] = Field(
+        alias="and", default=None
     )
+    """A list of filters or other logical operators to be combined using AND logic."""
+
+
+class V1LoadRequestQueryFilterLogicalOr(BaseModel):
+    or_: Optional[List["V1LoadRequestQueryFilterItem"]] = Field(
+        alias="or", default=None
+    )
+    """A list of filters or other logical operators to be combined using OR logic."""
+
+
+V1LoadRequestQueryFilterItem = Union[
+    V1LoadRequestQueryFilterBase,
+    V1LoadRequestQueryFilterLogicalOr,
+    V1LoadRequestQueryFilterLogicalAnd,
+]
+
+model_build_recursive(V1LoadRequestQueryFilterLogicalAnd)
+model_build_recursive(V1LoadRequestQueryFilterLogicalOr)
+
+
+V1LoadRequestQueryTimeDimensionGranularity = Literal[
+    "second",
+    "minute",
+    "hour",
+    "day",
+    "week",
+    "month",
+    "quarter",
+    "year",
+]
 
 
 class V1LoadRequestQueryTimeDimension(BaseModel):
-    dimension: str = Field(description="Time dimension to query")
-    granularity: Optional[str] = Field(
-        default=None, description="Granularity of the time dimension"
+    dimension: str
+    """The name of the time dimension."""
+
+    granularity: Optional[V1LoadRequestQueryTimeDimensionGranularity] = None
+    """Granularity level for the time dimension. 
+    Setting this to null will filter by the specified time dimension without grouping."""
+
+    date_range: Optional[Union[str, Tuple[str, str]]] = Field(
+        alias="dateRange", default=None
     )
-    date_range: Optional[Union[str, List[str]]] = Field(
-        alias="dateRange",
-        default=None,
-        description="Date range for the time dimension",
-    )
+    """Date range for filtering, either as a string in `YYYY-MM-DD` format 
+    or as a tuple of strings representing start and end dates."""
 
 
-try:
-    from pydantic import RootModel
+V1LoadRequestQueryOrderDirection = Literal["asc", "desc"]
 
-    V1LoadRequestQueryFilterItem = RootModel[  # type: ignore
-        Union[
-            V1LoadRequestQueryFilterBase,
-            V1LoadRequestQueryFilterLogicalOr,
-            V1LoadRequestQueryFilterLogicalAnd,
-        ]
-    ]
-
-except ImportError:
-
-    class V1LoadRequestQueryFilterItem(BaseModel):  # type: ignore
-        __root__: Union[
-            V1LoadRequestQueryFilterBase,
-            V1LoadRequestQueryFilterLogicalOr,
-            V1LoadRequestQueryFilterLogicalAnd,
-        ]
+V1LoadRequestQueryOrder = Union[
+    Dict[str, V1LoadRequestQueryOrderDirection],
+    List[str],
+]
 
 
 class V1LoadRequestQuery(BaseModel):
-    measures: Optional[List[str]] = Field(
-        default=None, description="List of measures to query"
-    )
-    dimensions: Optional[List[str]] = Field(
-        default=None, description="List of dimensions to query"
-    )
-    segments: Optional[List[str]] = Field(
-        default=None, description="List of segments to query"
-    )
+    measures: Optional[List[str]] = None
+    """List of measures to be queried."""
+
+    dimensions: Optional[List[str]] = None
+    """List of dimensions to be queried."""
+
+    segments: Optional[List[str]] = None
+    """List of segments to be used in the query. 
+    A segment is a named filter defined in the data model."""
+
     time_dimensions: Optional[List[V1LoadRequestQueryTimeDimension]] = Field(
-        alias="timeDimensions",
-        default=None,
-        description="List of time dimensions to query",
+        alias="timeDimensions", default=None
     )
-    order: Optional[List[List[str]]] = Field(
-        default=None, description="Order of the query result"
-    )
-    limit: Optional[int] = Field(
-        default=None, description="Limit for the query result"
-    )
-    offset: Optional[int] = Field(
-        default=None, description="Offset for the query result"
-    )
-    filters: Optional[List[V1LoadRequestQueryFilterItem]] = Field(
-        default=None, description="Filters for the query"
-    )
-    ungrouped: Optional[bool] = Field(
-        default=None, description="Indicates if the query is ungrouped"
-    )
+    """List of time dimensions to be used in the query."""
+
+    order: Optional[V1LoadRequestQueryOrder] = None
+    """Ordering criteria for the query, specified as a dictionary of measures 
+    or dimensions with `asc` or `desc` values."""
+
+    limit: Optional[int] = None
+    """Maximum number of rows to return in the query result."""
+
+    offset: Optional[int] = None
+    """Number of initial rows to skip in the query result. Default is 0."""
+
+    filters: Optional[List[V1LoadRequestQueryFilterItem]] = None
+    """List of filters to apply to the query."""
+
+    timezone: Optional[str] = None
+    """Time zone to be used for the query, specified in the TZ Database Name format 
+    (e.g., `America/Los_Angeles`)."""
+
+    renew_query: Optional[bool] = Field(alias="renewQuery", default=None)
+    """Whether to renew all refresh keys for queries and query results in the foreground. 
+    Default is false."""
+
+    total: Optional[bool] = None
+    """If true, Cube will run a total query and return the total number of rows, 
+    ignoring any row limit or offset. Default is false."""
+
+    ungrouped: Optional[bool] = None
+    """Indicates if the query should be ungrouped."""
+
+    query_type: Optional[str] = Field(alias="queryType", default=None)
+    """Type of the query, for internal use."""
 
 
 class V1LoadRequest(BaseModel):
-    query_type: Optional[str] = Field(
-        alias="queryType", default=None, description="Type of the query"
+    query_type: Optional[Literal["multi"]] = Field(
+        alias="queryType", default=None
     )
-    query: Optional[Union[V1LoadRequestQuery, List[V1LoadRequestQuery]]] = Field(
-        default=None, description="Query or list of queries"
-    )
+    """Specifies the type of the query."""
+
+    query: Optional[Union[V1LoadRequestQuery, List[V1LoadRequestQuery]]] = None
+    """The query or a list of queries to execute."""
 
 
 #############
--- src/cube_http/types/v1/meta.py
+++ src/cube_http/types/v1/meta.py
@@ -1,152 +1,246 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Literal, Optional, Union
 from pydantic import BaseModel, Field
 
 
 class V1CubeMetaJoin(BaseModel):
     name: str
-    relationship: str
-    sql: Optional[str] = Field(
-        default=None, description="SQL for join condition"
-    )
+    """The name of the joined cube. 
+    It must match the name of the cube being joined and follow the naming conventions."""
+
+    relationship: Literal[
+        "one_to_one",
+        "one_to_many",
+        "many_to_one",
+        "has_one",
+        "has_many",
+        "belongs_to",
+        "hasOne",
+        "hasMany",
+        "belongsTo",
+    ]
+    """The type of relationship between the cubes. 
+    Valid relationships are `one_to_one`, `one_to_many`, and `many_to_one`."""
+
+    sql: str
+    """The SQL ON clause that specifies the join condition between the cubes. 
+    It is important to accurately define the matching columns to ensure correct join behavior."""
 
 
 class V1CubeMetaSegment(BaseModel):
-    name: str
-    title: str
-    short_title: Optional[str] = Field(
-        alias="shortTitle",
-        default=None,
-        description="Short title of the segment",
-    )
-    sql: Optional[str] = Field(
-        default=None, description="SQL for segment condition"
-    )
+    name: str = ""
+    """The unique identifier for the segment. 
+    It must be unique among all segments, dimensions, and measures within a cube and follow naming conventions."""
+
+    sql: str = ""
+    """A required parameter that defines the SQL expression used to filter the data for this segment. 
+    The SQL should be valid within a WHERE clause and can implement complex filtering logic."""
+
+    public: Optional[bool] = None
+    """Controls the visibility of the segment. 
+    When set to `False`, the segment cannot be queried through the API. Defaults to `True`."""
 
 
 class V1CubeMetaDimension(BaseModel):
-    name: str
-    type: str
-    title: Optional[str] = Field(
-        default=None, description="Title of the dimension"
+    name: str = ""
+    """The unique identifier for the dimension. 
+    It must be unique among all dimensions, measures, and segments within a cube and follow naming conventions."""
+
+    title: Optional[str] = None
+    """A human-readable display name for the dimension. 
+    By default, Cube will humanize the dimension's name, but you can use this parameter to customize it."""
+
+    description: Optional[str] = None
+    """A description providing more context about what the dimension represents. 
+    This is useful for ensuring that users understand the attribute correctly."""
+
+    sql: str = ""
+    """A required parameter that defines the SQL expression used to derive the dimension's value. 
+    The SQL should return a value consistent with the dimension's type."""
+
+    type: Literal["time", "string", "number", "boolean", "geo"]
+    """A required parameter that specifies the type of data the dimension represents."""
+
+    format: Optional[
+        Literal["imageUrl", "id", "link", "currency", "percent"]
+    ] = None
+    """Optional parameter to define how the dimension's output should be formatted. 
+    Supported formats include `imageUrl`, `id`, `link`, `currency`, and `percent`."""
+
+    case: Optional[Dict[str, Any]] = None
+    """Optional parameter to define the dimension based on SQL conditions using a case statement. 
+    It allows for conditionally assigning labels based on the SQL conditions."""
+
+    meta: Optional[Dict[str, Any]] = None
+    """Custom metadata associated with the dimension, which can be used to pass additional information to the frontend."""
+
+    primary_key: Optional[bool] = Field(alias="primaryKey", default=None)
+    """Specifies if the dimension is the primary key for the cube. 
+    Setting this to `True` will change the default value of the `public` parameter to `False`."""
+
+    propagate_filters_to_sub_query: Optional[bool] = Field(
+        alias="propagateFiltersToSubQuery", default=None
     )
-    short_title: Optional[str] = Field(
-        alias="shortTitle",
-        default=None,
-        description="Short title of the dimension",
-    )
-    description: Optional[str] = Field(
-        default=None, description="Description of the dimension"
-    )
-    format: Optional[Union[str, Dict[str, Any]]] = Field(
-        default=None, description="Format of the dimension"
-    )
-    sql: Optional[str] = Field(default=None, description="SQL for the dimension")
-    primary_key: Optional[bool] = Field(
-        alias="primaryKey",
-        default=None,
-        description="If the dimension is a primary key",
-    )
-    public: Optional[bool] = Field(
-        default=None, description="If the dimension is public"
-    )
-    is_visible: Optional[bool] = Field(
-        alias="isVisible",
-        default=None,
-        description="Visibility of the dimension",
-    )
-    suggest_filter_values: Optional[bool] = Field(
-        alias="suggestFilterValues",
-        default=None,
-        description="Suggest filter values for the dimension",
-    )
+    """When set to `True`, the filters applied to the query will be passed to the subquery."""
+
+    public: Optional[bool] = None
+    """Controls the visibility of the dimension. 
+    When set to `False`, the dimension cannot be queried through the API. Defaults to `True`."""
+
+    sub_query: Optional[bool] = Field(alias="subQuery", default=None)
+    """Enables referencing a measure in a dimension by setting this to `True`. 
+    This is an advanced concept that allows for more complex dimension definitions."""
 
 
 class V1CubeMetaMeasure(BaseModel):
-    name: str
-    type: str
-    title: Optional[str] = Field(
-        default=None, description="Title of the measure"
-    )
-    short_title: Optional[str] = Field(
-        alias="shortTitle",
-        default=None,
-        description="Short title of the measure",
-    )
-    description: Optional[str] = Field(
-        default=None, description="Description of the measure"
-    )
-    sql: Optional[str] = Field(default=None, description="SQL for the measure")
-    agg_type: Optional[str] = Field(
-        alias="aggType",
-        default=None,
-        description="Aggregation type of the measure",
-    )
-    public: Optional[bool] = Field(
-        default=None, description="If the measure is public"
-    )
-    is_visible: Optional[bool] = Field(
-        alias="isVisible", default=None, description="Visibility of the measure"
-    )
-    cumulative: Optional[bool] = Field(
-        default=None, description="If the measure is cumulative"
-    )
-    cumulative_total: Optional[bool] = Field(
-        alias="cumulativeTotal",
-        default=None,
-        description="If the measure is cumulative total",
-    )
+    name: str = ""
+    """The unique identifier for the measure. 
+    It must be unique among all measures, dimensions, and segments within a cube and must follow naming conventions."""
+
+    title: Optional[str] = None
+    """A human-readable display name for the measure. 
+    By default, Cube will humanize the measure's name, but you can use this parameter to customize it."""
+
+    description: Optional[str] = None
+    """A description providing more context about what the measure represents. 
+    This is useful for ensuring that users understand the metric correctly."""
+
+    sql: str = ""
+    """A required parameter that defines the SQL expression used to calculate the measure. 
+    Depending on the measure type, this can be an aggregation, a simple SQL expression, or even a calculation involving other measures."""
+
+    type: Literal[
+        "string",
+        "time",
+        "boolean",
+        "number",
+        "count",
+        "count_distinct",
+        "count_distinct_approx",
+        "sum",
+        "avg",
+        "min",
+        "max",
+    ]
+    """A required parameter that specifies the type of aggregation or calculation the measure represents."""
+
+    format: Optional[Literal["percent", "currency"]] = None
+    """Defines how the measure's output should be formatted."""
+
+    filters: Optional[List[Dict[str, Any]]] = None
+    """Optional parameter for defining conditions that must be met for the measure's calculation. 
+    This is specified as an array of SQL conditions that are applied to the measure."""
+
     drill_members: Optional[List[str]] = Field(
-        alias="drillMembers",
-        default=None,
-        description="Drill members for the measure",
+        alias="drillMembers", default=None
     )
-    drill_members_grouped: Optional[Dict[str, List[str]]] = Field(
-        alias="drillMembersGrouped",
-        default=None,
-        description="Grouped drill members for the measure",
+    """An array of dimensions that can be used to drill down into the measure. 
+    These dimensions provide additional detail when exploring the measure's value."""
+
+    public: Optional[bool] = None
+    """Controls the visibility of the measure. 
+    When set to `False`, the measure cannot be queried through the API. Defaults to `True`."""
+
+    cumulative: Optional[bool] = None
+    """Indicates whether the measure is cumulative, meaning it accumulates values over time."""
+
+    cumulative_total: Optional[bool] = Field(
+        alias="cumulativeTotal", default=None
     )
+    """Indicates whether the measure represents a cumulative total."""
+
+    rolling_window: Optional[Dict[str, Union[str, int]]] = Field(
+        alias="rollingWindow", default=None
+    )
+    """Defines a rolling window for the measure, allowing the calculation of metrics within a specified time window. 
+    The window can be defined with trailing and leading parameters, and it only works with a single time dimension with a defined date range."""
+
+    meta: Optional[Dict[str, Any]] = None
+    """Custom metadata associated with the measure, which can be used to pass additional information to the frontend."""
 
 
 class V1CubeMeta(BaseModel):
     name: str
-    title: Optional[str] = Field(default=None, description="Title of the cube")
-    description: Optional[str] = Field(
-        default=None, description="Description of the cube"
+    """The unique identifier for the cube. 
+    Must be unique among all cubes and views within a deployment and follow the naming conventions."""
+
+    title: Optional[str] = None
+    """A human-readable display name for the cube. 
+    By default, Cube will humanize the cube's name (e.g., `users_orders` becomes `Users Orders`). 
+    Use this parameter to customize the display name if the default isn't suitable."""
+
+    description: Optional[str] = None
+    """A brief description of the cube to help your team understand its purpose and contents. 
+    Useful for ensuring that the data is interpreted correctly by users."""
+
+    sql_table: Optional[str] = Field(alias="sqlTable", default=None)
+    """Specifies the table in the database that this cube will query. 
+    It is a concise alternative to the `sql` parameter when querying entire tables."""
+
+    sql: Optional[str] = None
+    """The SQL query that generates the table to be queried by the cube. 
+    Typically takes the form of a `SELECT * FROM table` query. 
+    For simple table queries, prefer using the `sql_table` parameter."""
+
+    sql_alias: Optional[str] = Field(alias="sqlAlias", default=None)
+    """Custom SQL alias for the cube. Useful when the auto-generated alias is too long 
+    and may be truncated by the database."""
+
+    data_source: Optional[str] = Field(alias="dataSource", default=None)
+    """Specifies the data source name for the cube. 
+    This is useful when data should be fetched from multiple databases. 
+    The value is passed to the `driverFactory()` function as part of the context."""
+
+    public: Optional[bool] = None
+    """Controls the visibility of the cube. 
+    When set to `False`, the cube cannot be queried through the API. Defaults to `True`."""
+
+    is_visible: Optional[bool] = Field(alias="isVisible", default=None)
+    """Determines if the cube should be visible in the UI. 
+    If set to `False`, it will be hidden from the user interface."""
+
+    meta: Optional[Dict[str, Any]] = None
+    """Custom metadata for the cube. 
+    Can be used to pass additional information to the frontend."""
+
+    refresh_key: Optional[Dict[str, Union[str, Dict[str, str]]]] = Field(
+        alias="refreshKey", default=None
     )
-    file_name: Optional[str] = Field(
-        alias="fileName", default=None, description="File name of the cube"
-    )
-    public: Optional[bool] = Field(
-        default=None, description="If the cube is public"
-    )
-    is_visible: Optional[bool] = Field(
-        alias="isVisible", default=None, description="Visibility of the cube"
-    )
-    sql: Optional[str] = Field(
-        default=None, description="SQL query for the cube"
-    )
-    measures: List[V1CubeMetaMeasure]
-    dimensions: List[V1CubeMetaDimension]
-    segments: List[V1CubeMetaSegment]
-    joins: Optional[List[V1CubeMetaJoin]] = Field(
-        default=None, description="Joins for the cube"
-    )
+    """Defines the refresh key for the cube, used by Cube's caching layer to determine when the data should be refreshed.
+    Can be specified using SQL or an interval (e.g., `every: '1 hour'`)."""
+
+    extends: Optional[str] = None
+    """Allows extending another cube to reuse its declared members, such as `sql` or `measures`. 
+    Useful for creating variations of existing cubes without redefining all properties."""
+
+    measures: List["V1CubeMetaMeasure"]
+    """List of measures defined within the cube. 
+    Measures represent aggregated data points such as counts, sums, or averages."""
+
+    dimensions: List["V1CubeMetaDimension"]
+    """List of dimensions defined within the cube. 
+    Dimensions represent categorical data such as dates, strings, or numbers."""
+
+    segments: List["V1CubeMetaSegment"]
+    """List of segments defined within the cube. 
+    Segments represent predefined filters on the data, such as "Active Users"."""
+
+    joins: Optional[List["V1CubeMetaJoin"]] = None
+    """List of joins between this cube and other cubes. 
+    Joins define relationships between cubes, allowing for complex data retrievals."""
+
     pre_aggregations: Optional[List[Any]] = Field(
-        alias="preAggregations",
-        default=None,
-        description="Pre-aggregations for the cube",
+        alias="preAggregations", default=None
     )
-    meta: Optional[Dict[str, Any]] = Field(
-        default=None, description="Metadata of the cube"
-    )
+    """List of pre-aggregations defined for the cube. 
+    Pre-aggregations are used to speed up query performance by storing pre-computed results."""
+
     connected_component: Optional[int] = Field(
-        alias="connectedComponent",
-        default=None,
-        description="Connected component of the cube",
+        alias="connectedComponent", default=None
     )
+    """ID of the connected component associated with the cube. 
+    This is used internally for query planning and optimization."""
 
 
 class V1MetaResponse(BaseModel):
-    cubes: Optional[List[V1CubeMeta]] = Field(
-        default=None, description="List of cube metadata"
-    )
+    cubes: Optional[List[V1CubeMeta]] = None
+    """List of cube metadata in the response."""
