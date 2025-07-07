05f5c82358545692e00c7fc69482ad67377e5ba9
Michael Harris <mharris@definite.app>
2025-07-07 14:03:41 -0600

    chore(tests): add view to model fixtures

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -8,6 +8,8 @@ def test_get_metadata(url: str, token: str):
     meta = cube.v1.meta({"extended": True})
     assert meta.cubes
     assert len(meta.cubes) > 0
+    assert len([cube for cube in meta.cubes if cube.type == "cube"]) > 0
+    assert len([view for view in meta.cubes if view.type == "view"]) > 0
 
 
 @pytest.mark.asyncio
@@ -16,3 +18,5 @@ async def test_async_get_metadata(url: str, token: str):
     meta = await cube.v1.meta()
     assert meta.cubes
     assert len(meta.cubes) > 0
+    assert len([cube for cube in meta.cubes if cube.type == "cube"]) > 0
+    assert len([view for view in meta.cubes if view.type == "view"]) > 0
